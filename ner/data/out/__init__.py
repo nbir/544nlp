08a5ed0c0905ca42fb0bb6663a68af9ad5c11599
Nibir Bora <nbora@usc.edu>
2013-02-02 18:08:50 -0800

    small changes

--- ner/data/out/__init__.py
+++
@@ -1,6 +0,0 @@
-# Named Entity Recognizer - 544 NLP Assignment1
-#
-# Copyright (C) 2013, Nibir Bora
-# Author: Nibir Bora <nbora@usc.edu>
-# URL: <http://nibir.me>
-# For license information, see LICENSE
