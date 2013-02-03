7516d9e1f3773b63b03e397afad639e44d10baea
Nibir Bora <nbora@usc.edu>
2013-02-02 17:57:45 -0800

    repo created

---
+++ ner/lib/__init__.py
@@ -0,0 +1,6 @@
+# Named Entity Recognizer - 544 NLP Assignment1
+#
+# Copyright (C) 2013, Nibir Bora
+# Author: Nibir Bora <nbora@usc.edu>
+# URL: <http://nibir.me>
+# For license information, see LICENSE
